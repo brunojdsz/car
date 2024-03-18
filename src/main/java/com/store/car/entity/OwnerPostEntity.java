bca85113d652fc1da9612cdd36a3aeeecdb0a644
brunojdsz <bruno.bjwm@gmail.com>
2024-03-18 10:25:32 -0300

    Finalizado microservico Car com Kafka

---
+++ src/main/java/com/store/car/entity/OwnerPostEntity.java
@@ -0,0 +1,22 @@
+package com.store.car.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "owner_post")
+@Data
+@NoArgsConstructor
+public class OwnerPostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "owner_name")
+    private String name;
+    @Column(name = "owner_type")
+    private String type;
+    @Column(name = "owner_Contact")
+    private String contactNumber;
+}
